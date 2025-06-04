c863c21dfd7656038c10b9257add668fd00a3cf9
frode-o <209454005+frode-o@users.noreply.github.com>
2025-06-04 19:34:33 +0200

    Add JS and HTML files
    Add the JavaScript.js and Rock-paper-scissors.html files. Import js file
    in html file. Add pseudo code in js file.

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+// Create variables for computerChoice, userChoice og winner.
+// Get computer choice and store it in computerChoice.
+// 	Declare variabel for randomInteger.
+//	Get random number between 0 and 2,9999999
+//	Transform number to integer between 0 and 2.
+//	Store integer in variable randomInteger.
+//	If randomInteger is 0, return rock in computerChoice.
+//	If randomInteger is 1, return paper in computerChoice.
+//	If randomInteger is 2, return scissors in computerChoice.
+// 	Return computerChoice.
+// Get user choice and store it in userChoice.
+// 	Declare variable userInput.
+// 	Prompt user for choice.
+// 	Store input in userInput.
+// 	Make lowercase and store in userChoice.
+// 	Validate that choice is either rock, paper or scissors.
+// 	If not, print error.
+// 	Else return userChoice.
+// Draw the winner.
+// Print userChoice.
+// Print computerChoice.
+// Announce the winner.
